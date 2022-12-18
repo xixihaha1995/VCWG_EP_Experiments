4d38710c6e97492feb1b1ebc7d6a3dc8be4a58ec
Lichen <wulicheneason@gmail.com>
2022-12-17 20:00:30 -0700

    to re-generate all cases with shading surfaces

--- _1_Main_SensitivityAnalysis.py
+++ _1_Main_SensitivityAnalysis.py
@@ -23,12 +23,11 @@ def batch_run(ini_files):
             p.join()
 def for_loop_all_ini():
     selected_jobs = ["BUBBLE_Ue1.ini","BUBBLE_Ue2.ini",
-                     "Vancouver_Rural.ini","Vancouver_TopForcing.ini",
-                     "CAPITOUL_WithCooling.ini","CAPITOUL_WithoutCooling.ini",]
+                     "Vancouver_Rural.ini","Vancouver_TopForcing.ini",]
     # selected_jobs = ["BUBBLE_Ue1.ini","BUBBLE_Ue2.ini"]
     # selected_jobs = ["CAPITOUL_WithCooling.ini"]
-    selected_jobs = ["BUBBLE_Ue1.ini", "BUBBLE_Ue2.ini" ]
-    selected_jobs = ['Chicago_MedOffice_Detailed.ini', 'Chicago_MedOffice_ShoeBox.ini']
+    # selected_jobs = ["BUBBLE_Ue1.ini", "BUBBLE_Ue2.ini" ]
+    # selected_jobs = ['Chicago_MedOffice_Detailed.ini', 'Chicago_MedOffice_ShoeBox.ini']
     nbr_job_for_one_batch = 6
     for i in range(0,len(selected_jobs),nbr_job_for_one_batch):
         print('Todo jobs',selected_jobs[i:i+nbr_job_for_one_batch])
