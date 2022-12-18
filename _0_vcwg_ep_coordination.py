e479aef554c261f0222061bc85c27ebaaa36ad1f
Lichen <wulicheneason@gmail.com>
2022-12-17 20:56:00 -0700

    regenerated all cases with shading

--- _0_vcwg_ep_coordination.py
+++ _0_vcwg_ep_coordination.py
@@ -21,7 +21,7 @@ def ini_all(sensitivity_file_name):
     vcwg_hConv_w_m2_per_K = 10
     config = configparser.ConfigParser()
     project_path = os.path.dirname(os.path.abspath(__file__))
-    config_path = os.path.join(project_path, 'A_prepost_processing','configs','bypass',sensitivity_file_name)
+    config_path = os.path.join(project_path, 'A_prepost_processing','configs','bypass','AllCases',sensitivity_file_name)
     config.read(config_path)
     bld_type = config['Bypass']['bld_type']
     experiments_theme = config['Bypass']['experiments_theme']
