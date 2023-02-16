2a2f3e4c0ca47f923005c4335ca679f6835978c4
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2023-02-16 13:45:14 -0700

    looks like the core zone

--- A_prepost_processing/_14_TimeSeries_Energy.py
+++ A_prepost_processing/_14_TimeSeries_Energy.py
@@ -6,6 +6,108 @@ import sqlite3
 import numpy as np
 from matplotlib import pyplot as plt
 
+def get_zone_to_pthp_dict():
+    zone_pthp_dict = {}
+    zone_pthp_dict[1] = 56
+    zone_pthp_dict[10] = 65
+    zone_pthp_dict[100] = 6
+    zone_pthp_dict[11] = 66
+    zone_pthp_dict[12] = 67
+    zone_pthp_dict[13] = 68
+    zone_pthp_dict[14] = 69
+    zone_pthp_dict[15] = 70
+    zone_pthp_dict[16] = 71
+    zone_pthp_dict[17] = 72
+    zone_pthp_dict[18] = 73
+    zone_pthp_dict[19] = 74
+    zone_pthp_dict[2] = 57
+    zone_pthp_dict[20] = 75
+    zone_pthp_dict[21] = 76
+    zone_pthp_dict[22] = 77
+    zone_pthp_dict[23] = 78
+    zone_pthp_dict[24] = 79
+    zone_pthp_dict[25] = 80
+    zone_pthp_dict[26] = 81
+    zone_pthp_dict[27] = 82
+    zone_pthp_dict[28] = 83
+    zone_pthp_dict[29] = 84
+    zone_pthp_dict[3] = 58
+    zone_pthp_dict[30] = 85
+    zone_pthp_dict[31] = 86
+    zone_pthp_dict[32] = 87
+    zone_pthp_dict[33] = 88
+    zone_pthp_dict[34] = 89
+    zone_pthp_dict[35] = 90
+    zone_pthp_dict[36] = 91
+    zone_pthp_dict[37] = 92
+    zone_pthp_dict[38] = 93
+    zone_pthp_dict[39] = 94
+    zone_pthp_dict[4] = 59
+    zone_pthp_dict[40] = 95
+    zone_pthp_dict[41] = 96
+    zone_pthp_dict[42] = 97
+    zone_pthp_dict[43] = 98
+    zone_pthp_dict[44] = 99
+    zone_pthp_dict[45] = 100
+    zone_pthp_dict[46] = 38
+    zone_pthp_dict[47] = 39
+    zone_pthp_dict[48] = 40
+    zone_pthp_dict[49] = 41
+    zone_pthp_dict[5] = 60
+    zone_pthp_dict[50] = 42
+    zone_pthp_dict[51] = 43
+    zone_pthp_dict[52] = 44
+    zone_pthp_dict[53] = 45
+    zone_pthp_dict[54] = 46
+    zone_pthp_dict[55] = 47
+    zone_pthp_dict[56] = 48
+    zone_pthp_dict[57] = 49
+    zone_pthp_dict[58] = 50
+    zone_pthp_dict[59] = 51
+    zone_pthp_dict[6] = 61
+    zone_pthp_dict[60] = 52
+    zone_pthp_dict[61] = 53
+    zone_pthp_dict[62] = 54
+    zone_pthp_dict[63] = 55
+    zone_pthp_dict[64] = 36
+    zone_pthp_dict[65] = 37
+    zone_pthp_dict[66] = 34
+    zone_pthp_dict[67] = 35
+    zone_pthp_dict[68] = 25
+    zone_pthp_dict[69] = 26
+    zone_pthp_dict[7] = 62
+    zone_pthp_dict[70] = 27
+    zone_pthp_dict[71] = 28
+    zone_pthp_dict[72] = 29
+    zone_pthp_dict[73] = 30
+    zone_pthp_dict[74] = 31
+    zone_pthp_dict[75] = 32
+    zone_pthp_dict[76] = 33
+    zone_pthp_dict[77] = 23
+    zone_pthp_dict[78] = 24
+    zone_pthp_dict[79] = 18
+    zone_pthp_dict[8] = 63
+    zone_pthp_dict[80] = 19
+    zone_pthp_dict[81] = 20
+    zone_pthp_dict[82] = 21
+    zone_pthp_dict[83] = 22
+    zone_pthp_dict[84] = 16
+    zone_pthp_dict[85] = 17
+    zone_pthp_dict[86] = 14
+    zone_pthp_dict[87] = 15
+    zone_pthp_dict[88] = 12
+    zone_pthp_dict[89] = 13
+    zone_pthp_dict[9] = 64
+    zone_pthp_dict[90] = 11
+    zone_pthp_dict[91] = 7
+    zone_pthp_dict[92] = 8
+    zone_pthp_dict[93] = 9
+    zone_pthp_dict[94] = 10
+    zone_pthp_dict[95] = 1
+    zone_pthp_dict[96] = 2
+    zone_pthp_dict[97] = 3
+    zone_pthp_dict[98] = 4
+    zone_pthp_dict[99] = 5
 
 def read_sql(sql_path):
     sql_report_name = 'AnnualBuildingUtilityPerformanceSummary'
@@ -21,18 +123,22 @@ def read_sql(sql_path):
         cursor = con.cursor()
     #Zone Packaged Terminal Heat Pump Total Heating Energy
     time_series_cooling_q = f"SELECT * FROM ReportVariableWithTime " \
-                            f"WHERE Name = 'Zone Packaged Terminal Heat Pump Total Heating Energy'"
-    time_series_results = cursor.execute(time_series_cooling_q).fetchall()
+                            f"WHERE Name = 'Zone Packaged Terminal Heat Pump Total Cooling Energy'"\
+                            f"And KeyValue = 'PTHP 59'"
+    time_series_results_cooling = cursor.execute(time_series_cooling_q).fetchall()
     # # time_series_results is a list of tuples, to sum over the tuple[4]
     # cooling_sum_J = sum([_tuple[4] for _tuple in time_series_results])
 
-    # time_series_electricity_q = f"SELECT * FROM ReportVariableWithTime " \
-    #                         f"WHERE Name = 'Zone Packaged Terminal Heat Pump Electricity Energy'"
-    # time_series_results = cursor.execute(time_series_electricity_q).fetchall()
+    time_series_electricity_q = f"SELECT * FROM ReportVariableWithTime " \
+                            f"WHERE Name = 'Zone Packaged Terminal Heat Pump Electricity Energy'"\
+                            f"And KeyValue = 'PTHP 5'"
+    time_series_results_elec = cursor.execute(time_series_electricity_q).fetchall()
     # time_series_results is a list of tuples, to sum over the tuple[4]
-    elec_lst = [(_tuple[4]) for _tuple in time_series_results]
+    elec_lst = [(time_series_results_elec[i][4])
+                for i in range(len(time_series_results_elec))]
     zone_oat_hourly_q = f"SELECT * FROM ReportVariableWithTime " \
-                        f"WHERE Name = 'Zone Outdoor Air Drybulb Temperature'"
+                        f"WHERE Name = 'Zone Outdoor Air Drybulb Temperature'"\
+                        f"And KeyValue = 'THERMAL ZONE 99'"
     time_series_results = cursor.execute(zone_oat_hourly_q).fetchall()
     zone_oat_hourly = [(_tuple[4]) for _tuple in time_series_results]
     return elec_lst, zone_oat_hourly
@@ -41,9 +147,9 @@ def read_sql(sql_path):
 
 def plot_elec(scalar_lst, vector_lst, tilte):
     pass
-    # The each 100 values is for one timestep
-    scalar_lst = [sum(scalar_lst[i:i + 100])/100 for i in range(0, len(scalar_lst), 100)]
-    vector_lst = [sum(vector_lst[i:i + 100])/100 for i in range(0, len(vector_lst), 100)]
+    # # The each 100 values is for one timestep
+    # scalar_lst = [sum(scalar_lst[i:i + 100])/100 for i in range(0, len(scalar_lst), 100)]
+    # vector_lst = [sum(vector_lst[i:i + 100])/100 for i in range(0, len(vector_lst), 100)]
     # for plotting purposes, convert lst to np array
     scalar_lst = np.array(scalar_lst)
     vector_lst = np.array(vector_lst)
@@ -68,10 +174,13 @@ def main():
     experiments_folder = os.path.join(_base, _sub_folder,'eplusout.sql')
     scalar_elec_lst, scalar_oat_lst = read_sql(experiments_folder)
     _sub_folder = 'Vector_Mod_Cmu'
+    _sub_folder = 'Vector_Default_Cmu'
     experiments_folder = os.path.join(_base, _sub_folder, 'eplusout.sql')
     vector_elec_lst, vector_oat_lst = read_sql(experiments_folder)
-    plot_elec(scalar_oat_lst, vector_oat_lst, 'Zone Outdoor Air Drybulb Temperature (100 Zone, Avg, C, Hourly)')
-    plot_elec(scalar_elec_lst, vector_elec_lst, 'Zone PTHP Cooling Energy (100 Zone, Sum, J, 5 min)')
+    plot_elec(scalar_oat_lst, vector_oat_lst, 'Zone Outdoor Air Drybulb Temperature (Zone 1, C, Hourly)')
+    plot_elec(scalar_elec_lst, vector_elec_lst, 'Zone PTHP Elec Energy (Zone 1, 5 min)\n'
+                                                f'Scalar {round(sum(scalar_elec_lst)/1E9, 2)} GJ, '
+                                                f'Vector {round(sum(vector_elec_lst)/1E9, 2)} GJ')
 
 if __name__ == '__main__':
     main()
